ac55b17b41e711d36c2b68445e693371a2cbd66c
Dmitry Timofeev <dimadimajob@gmail.com>
2024-05-20 18:35:32 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
+// import { ReactComponent as ReactLogo } from './assets/images/react.svg';
+
+console.log(style)
